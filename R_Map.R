3659ae366667f7879eeb7a6ee58ebc710401223d
Yunjing Hu <ucfnyhu@ucl.ac.uk>
2018-12-24 18:39:19 +0000

    tmap added

--- R_Map.R
+++ R_Map.R
@@ -5,11 +5,11 @@ library(rgdal)
 library(leaflet)
 library(htmltools)
 library(RColorBrewer)
+library(shinyjs)
 
 # read the London Borough boundary shapefile and census csv file from a local directory
-BoroughBd <- readOGR("C:/Users/alexy/Desktop/GIS/wk1/BoundaryDataEdinaCensus/england_lad_2011.shp")
-LondonData <- read.csv("C:/Users/alexy/Desktop/GIS/wk1/LondonData.csv")
-LondonData <- read_csv("C:/Users/alexy/Desktop/GIS/wk1/LondonData.csv", na = "n/a")
+BoroughBd <- readOGR("england_lad_2011.shp")
+LondonData <- read.csv("LondonData.csv")
 
 # reorganize the data
 LondonData <- data.frame(LondonData)
@@ -22,8 +22,7 @@ names(BoroughBd)[6] <- c("Percentage")
 BoroughBd <- BoroughBd[c(3,6)] # extract the two neccessary columns
 Borough_repro <-spTransform(BoroughBd, CRS("+proj=longlat +datum=WGS84")) #reproject the data
 
-#plot the Borough data
-# add a base map layer in esri grayscale style
+#plot the Borough data with leaflet
 labels <- sprintf("<strong>%s</strong><br/>%g percent",Borough_repro$`Borough Name`,Borough_repro$Percentage) %>% lapply(htmltools::HTML)
 bins <- c(10.3, 19.9, 31.0, 39.4, 48.2, 55.1)
 pal <- colorBin(c("#f7fbff", "#c8ddf0","#73b3d8","#2879b9","#08306b"), domain = Borough_repro$Percentage, bins = bins)
@@ -43,3 +42,14 @@ map%>%addPolygons(weight = 1,opacity = 1, color = "#fff", smoothFactor = 0.3, fi
                     direction = "auto"))
 map%>%addLegend(pal = pal, values = ~Percentage, opacity = 0.7, title = "% people not born in UK",
                 position = "bottomright")
+
+#plot the map with tmap
+tmap_mode("view")
+tm_shape(Borough_repro) +
+  tm_polygons("Percentage",
+              style="jenks",
+              palette=get_brewer_pal("Blues", n = 5,contrast = c(0,1)),
+              border.col = "white",
+              midpoint=NA,
+              popup.vars="Percentage",
+              title="% of people not born in London")
