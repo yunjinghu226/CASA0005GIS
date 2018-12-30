f5e51e4fd723bbd6dee00e67c950c5209e7d6081
Yunjing Hu <ucfnyhu@ucl.ac.uk>
2018-12-30 21:40:47 +0000

    data prep

--- Part3/grapher.R
+++
@@ -1,8 +0,0 @@
-# a function adding the zscore column to the lsoa attribute table
-# arguments: weighting values (12)
-# output: an updated spatial polygon dataframe
-# available data: variable_zscore.csv, LSOA_2011_London_gen_MHW.shp
-
-grapher <- function(x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12){
-  
-}
